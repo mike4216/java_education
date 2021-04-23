ffc5d0568de80bc98b660983b1e5311dd3129872
Mikhail <m.lestchenko@gmail.com>
2021-04-23 19:58:51 +0300

    init

---
+++ src/main/java/com/github/mike4216/instagramApplication.java
@@ -0,0 +1,13 @@
+package com.github.mike4216;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class instagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(instagramApplication.class, args);
+	}
+
+}
