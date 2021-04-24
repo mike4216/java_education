5df85afe1e5c75059aab0d9c93ae2693cb1304fa
Mikhail <m.lestchenko@gmail.com>
2021-04-24 20:43:29 +0300

    created json web token success response and message response objects

---
+++ src/main/java/com/github/mike4216/demo/payload/response/JWTTokenSuccessResponse.java
@@ -0,0 +1,11 @@
+package com.github.mike4216.demo.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class JWTTokenSuccessResponse {
+    private boolean success;
+    private String token;
+}
