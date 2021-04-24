f2a7f6a403d32e87f8481ccbaf26e11aab8d1b01
Mikhail <m.lestchenko@gmail.com>
2021-04-24 19:41:54 +0300

    created validation class for check password matches (for password check annotation)

---
+++ src/main/java/com/github/mike4216/demo/validations/PasswordMatchesValidator.java
@@ -0,0 +1,20 @@
+package com.github.mike4216.demo.validations;
+
+import com.github.mike4216.demo.annotations.PasswordMatches;
+import com.github.mike4216.demo.payload.request.SignUpRequest;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext constraintValidatorContext) {
+        SignUpRequest userSignUpRequest = (SignUpRequest) obj;
+        return userSignUpRequest.getPasswords().equals(userSignUpRequest.getConfirmPassword());
+    }
+
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+        ConstraintValidator.super.initialize(constraintAnnotation);
+    }
+}
