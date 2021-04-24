7c5351338c235bf65ec4fba4904a5ba95a054cd1
Mikhail <m.lestchenko@gmail.com>
2021-04-24 17:19:52 +0300

    created authentication entry point

---
+++ src/main/java/com/github/mike4216/demo/security/JWTAuthenticationEntryPoint.java
@@ -0,0 +1,28 @@
+package com.github.mike4216.demo.security;
+
+import com.github.mike4216.demo.payload.response.InvalidLoginResponse;
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JWTAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest,
+                         HttpServletResponse httpServletResponse,
+                         AuthenticationException e
+    ) throws IOException, ServletException {
+        InvalidLoginResponse loginresponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(loginresponse);
+        httpServletResponse.setContentType(SecurityConstants.CONTENT_TYPE);
+        httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+        httpServletResponse.getWriter().println(jsonLoginResponse);
+    }
+}
