e7a9bfa8e23f9614133de301e941868e53507fb1
Mikhail <m.lestchenko@gmail.com>
2021-04-24 20:45:21 +0300

    created methods for user's authentication  and registration

--- src/main/java/com/github/mike4216/demo/web/AuthController.java
+++ src/main/java/com/github/mike4216/demo/web/AuthController.java
@@ -1,9 +1,25 @@
 package com.github.mike4216.demo.web;
 
+import com.github.mike4216.demo.payload.request.LoginRequest;
+import com.github.mike4216.demo.payload.response.JWTTokenSuccessResponse;
+import com.github.mike4216.demo.payload.response.MessageResponse;
+import com.github.mike4216.demo.payload.request.SignUpRequest;
+import com.github.mike4216.demo.security.JWTTokenProvider;
+import com.github.mike4216.demo.security.SecurityConstants;
+import com.github.mike4216.demo.service.UserService;
+import com.github.mike4216.demo.validations.ResponseErrorValidation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.ObjectUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @CrossOrigin
 @RestController
@@ -11,5 +27,38 @@ import org.springframework.web.bind.annotation.RestController;
 @PreAuthorize("permitAll()")
 public class AuthController {
 
+    @Autowired
+    private JWTTokenProvider jwtTokenProvider;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private ResponseErrorValidation responseErrorValidation;
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signin")
+    public ResponseEntity<Object> authenticateUser(@Valid @RequestBody LoginRequest loginRequest,
+                                                   BindingResult bindingResult){
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                loginRequest.getUsername(),
+                loginRequest.getPassword()
+        ));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = SecurityConstants.TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTTokenSuccessResponse(true, jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> registerUser(@Valid @RequestBody SignUpRequest signUpRequest,
+                                               BindingResult bindingResult){
+        ResponseEntity<Object> errors = responseErrorValidation.mapValidationService(bindingResult);
+        if (!ObjectUtils.isEmpty(errors)) return errors;
+        userService.createUser(signUpRequest);
+        return ResponseEntity.ok(new MessageResponse("User registered successfully"));
+    }
 
 }
