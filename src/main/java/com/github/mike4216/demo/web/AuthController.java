b970c57643d52e11bad3c73768614e5e42aa077a
Mikhail <m.lestchenko@gmail.com>
2021-04-24 20:14:15 +0300

    create auth controller and validator for   response errors

---
+++ src/main/java/com/github/mike4216/demo/web/AuthController.java
@@ -0,0 +1,15 @@
+package com.github.mike4216.demo.web;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("/api/auth")
+@PreAuthorize("permitAll()")
+public class AuthController {
+
+
+}
